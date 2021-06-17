d68e3513fa86233be9e6e0ac5ca78e7174a50fa5
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-17 08:03:31 -0700

    Using the write() function of a c lang

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar,
+fwrite("and that piece of art is useful\" - Dora Korpar,\0
 2015-10-19\n", 59, 1, stderr);
 return (1);
 }
