1eaf076c564f58ce03d9048bea70c90cab30afef
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-17 06:21:58 -0700

    Extracting the intel syntax of a c code

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,8 +1,10 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
 	.section	.rodata
+	.align 8
 .LC0:
-	.string	"Hello World!"
+	.string	"Programming is like building a multilingual puzzle\n"
 	.text
 	.globl	main
 	.type	main, @function
@@ -10,15 +12,16 @@ main:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
+	lea	rdi, .LC0[rip]
 	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
+	call	getchar@PLT
+	mov	eax, 0
+	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
