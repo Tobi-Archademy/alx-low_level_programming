f998f74aab2057c161ef4c70656be0ab6c17ab2c
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-17 03:17:33 -0700

    Coding in c lang using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main (void)
 {
-puts("Programming is like building a multilingual puzzle\n", stderr);
+puts("Programming is like building a multilingual puzzle\n");
 getchar();
 return (0);
 }
