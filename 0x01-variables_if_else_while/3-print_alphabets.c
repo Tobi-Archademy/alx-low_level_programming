86183dcac7ddefa3e72d71d51efd5f95c4e8dafe
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-18 11:55:07 -0700

    Using putchar to write upper&lower_case letters in c lang

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point fucntion
+ *
+ * Description: prints all lowercase characters, followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+putchar(ch);
+for (ch = 65; ch <= 90; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
