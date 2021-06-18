56f5a4c7f046305fe0e6a4a5697834fe16036bb8
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-18 12:25:36 -0700

    Using putchar to write numbers in c lang

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point fucntion
+ *
+ * Description: prints all lowercase characters, followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%i", i);
+putchar('\n');
+return (0);
+}
