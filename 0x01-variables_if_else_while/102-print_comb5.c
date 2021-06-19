c24ff81ae9d5258dfdd1cc0c2cea4485a55912aa
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-19 09:49:48 -0700

    Combining numbers using putchar

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - Prints 3 combination of numbers
+ * main - Entry point
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int c, i, k, j;
-for (c = 48; c <= 57; c++)
+int i; /*Decarling statement*/
+int j;
+/*for 0-9 with ASCII*/
+for (i = 0; i < 100 ; i++)
 {
-for (i = 48; i <= 57; i++)
+for (j = i + 1 ; j < 100; j++)
 {
-for (k = 48; k <= 57; k++)
-{
-for (j = 48 + 1; j <= 57; j++)
-{
-if (((k + j) > (c + i) &&  k >= c) || c < k)
-{
-putchar(c);
-putchar(i);
-putchar(' ');
-putchar(k);
-putchar(j);
-if (c + i + k + j == 227 && c == 57)
+putchar (i / 10 + '0'); /*print  first nums, first digit*/
+putchar (i % 10 + '0'); /*print  first nums, second digit*/
+putchar (' '); /*print a space*/
+putchar (j / 10 + '0'); /*print  sencond nums, first digit*/
+putchar (j % 10 + '0'); /*print  second nums, second digit*/
+/*break program if the condition is true (if is 98 99)*/
+if (i == 98 && j == 99)
 {
 break;
 }
-else
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
-putchar('\n');
+putchar(','); /*print (,)*/
+putchar (' '); /*print a space*/
+} /*end for #2*/
+} /*end for #1*/
+putchar('\n'); /*new line*/
 return (0);
 }
