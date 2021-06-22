1ab2588e40402ba760ae17258325867f50fd53a4
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-21 23:30:16 -0700

    Alphabets in small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+
+}
