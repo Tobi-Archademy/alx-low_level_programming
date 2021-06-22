163896286e5639ff20aa4d13800a7c379d217956
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-22 09:19:43 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-
 int main(void)
 {
 int ch;
 for (ch = 97; ch <= 122; ch++)
-putchar(ch);
-putchar('\n');
+_putchar(ch);
+_putchar('\n');
 return (0);
 
 }
