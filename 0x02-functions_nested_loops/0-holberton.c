0369f77d801712fc2b0085b4940859636d2fa815
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-22 01:02:50 -0700

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+}
