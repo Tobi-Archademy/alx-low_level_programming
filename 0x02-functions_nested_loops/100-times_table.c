9dbe36fe0f10a78ed09cb628c2792354cdf6862c
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-23 10:55:57 -0700

    printing times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,13 +17,21 @@ for (b = 1; b <= n; b++)
 c = a * b;
 _putchar(44);
 _putchar(32);
-if (c <= n)
+if (c <= 9)
 {
+_putchar(32);
 _putchar(32);
 _putchar(c + 48);
 }
+else if (c <= 99)
+{
+_putchar(32);
+_putchar((c / 10) + 48);
+_putchar((c % 10) + 48);
+}
 else
 {
+_putchar(((c / 100) % 10) + 48);
 _putchar((c / 10) + 48);
 _putchar((c % 10) + 48);
 }
