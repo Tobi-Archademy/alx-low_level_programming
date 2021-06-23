f85be4e2525795f192c43e9d8e011308a013efa2
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-23 11:02:28 -0700

    printing times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
+
 /**
- * print_times_table - that prints the 9 times table
- * @n: is an integer argument
- * Return: no return
- */
+* print_times_table - Prints the n times table
+*
+* @n: number times table (0 < n <= 15)
+*
+* Return: no return
+*/
 void print_times_table(int n)
 {
-int a, b, c;
+int a, b, op;
+
 if (n >= 0 && n <= 15)
 {
 for (a = 0; a <= n; a++)
@@ -14,29 +18,30 @@ for (a = 0; a <= n; a++)
 _putchar(48);
 for (b = 1; b <= n; b++)
 {
-c = a * b;
+op = a * b;
 _putchar(44);
 _putchar(32);
-if (c <= 9)
+if (op <= 9)
 {
 _putchar(32);
 _putchar(32);
-_putchar(c + 48);
+_putchar(op + 48);
 }
-else if (c <= 99)
+else if (op <= 99)
 {
 _putchar(32);
-_putchar((c / 10) + 48);
-_putchar((c % 10) + 48);
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
 }
 else
 {
-_putchar(((c / 100) % 10) + 48);
-_putchar((c / 10) + 48);
-_putchar((c % 10) + 48);
-}
+_putchar(((op / 100) % 10) + 48);
+_putchar(((op / 10) % 10) + 48);
+_putchar((op % 10) + 48);
 }
 }
 _putchar('\n');
 }
 }
+}
+
