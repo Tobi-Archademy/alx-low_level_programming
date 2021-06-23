a4c9de26fc53ca03b96f13c74888652c703ce2ce
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-22 23:38:21 -0700

    adding using int_add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - a function used to add two integers.
+ *
+ * @a: stores the variable of the datatype to be sum up.
+ * @b: stores the variable of the datatype to be sum up.
+ * Return: no return
+ */
+int add(int a, int b)
+{
+int x;
+x = a + b;
+return (x);
+}
