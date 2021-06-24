5def52f6d4752b6bce98d1e0d001caf91280dbd2
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-24 11:45:38 -0700

    skipping numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - a function used to print numbers, skipping 2 and 4.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int n = 0;
+while (n < 10)
+{
+
+if
+{
+(n == 2) || (n == 4)
+n++;
+continue;
+}
+
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
