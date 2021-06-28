e14f18ab2af9810376fe324c0a7381aa57ad14ec
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-28 07:23:15 -0700

    Printing out a string to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts - a function used to print out a string on stdout.
+ * @str: hold the value of a string.
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+_puts(str);
+}
