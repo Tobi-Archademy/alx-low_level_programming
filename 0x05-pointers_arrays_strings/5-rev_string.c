48f5f1d03742feaf5ea9a69f567a8ec4d1a8c8d5
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-28 10:19:40 -0700

    reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,12 +14,13 @@ char store;
 while (*end)
 {
 end++; /* set pos of end pointer to point to last element */
-end = end - 1; /* set end pos to end -1 b/c end is really '\0' */
 }
+end = end - 1; /* set end pos to end -1 b/c end is really '\0' */
+
 while (s < end) /* stop once we get to last address */
 {
 store = *s; /* set store ch to s[currentPos] */
-s++ = *end; /* set position of s = pos end, and increment s */
-end-- = store; /* set pos of end to stored char and increment */
+*s++ = *end; /* set position of s = pos end, and increment s */
+*end-- = store; /* set pos of end to stored char and increment */
 }
 }
