0c648d77b3efb0d37ba6074a3b359310f32e78e5
Oluwatobi <oluwatobiabass5@gmail.com>
2021-06-30 11:38:23 -0700

    changing strings to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *string_toupper - a function that changes a string to uppercase
+ *
+ *@ptr: a pointer to a variable of the type char
+ *
+ *Return: dest data
+ */
+char *string_toupper(char *ptr)
+{
+char str[], *ptr;
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+i++;
+}
+*ptr = str[i];
+}
