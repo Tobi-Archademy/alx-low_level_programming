0d7b8b50efb5a9e69264bd0f7bf6a0cd63407983
Oluwatobi <oluwatobiabass5@gmail.com>
2021-07-05 11:42:43 -0700

    function to copy a memory block from one string to another

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -46,6 +46,7 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 #endif
