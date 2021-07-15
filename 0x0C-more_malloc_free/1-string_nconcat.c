cd7b490ad1c75a09215d0c4b19f3d4984c654b53
Oluwatobi <oluwatobiabass5@gmail.com>
2021-07-15 13:39:17 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,42 @@
 #include "holberton.h"
 #include <stdlib.h>
+
 /**
- * string_nconcat - Function that concatenates two strings.
- * @s1: String 1.
- * @s2: String 2.
- * @n: Bytes from s2.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
  *
- * Return: Pointer to the array or NULL.
+ * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0, j = 0, k = 0, l = 0, m = 0, q = 0;
-char *ptr;
+unsigned int i, j, k;
+char *s;
+
 if (s1 == NULL)
+i = 0;
+else
 {
-s1 = "";
+for (i = 0; s1[i]; i++)
+;
 }
 if (s2 == NULL)
+j = 0;
+else
 {
-s2 = NULL;
+for (j = 0; s2[j]; j++)
+;
 }
-while (*(s1 + i))
-{
-i++;
-}
-while (*(s2 + j))
-{
-j++;
-}
-j++;
-
-ptr = malloc(sizeof(char) * (i + j));
-if (ptr == NULL)
-{
+if (j > n)
+j = n;
+s = malloc(sizeof(char) * (i + j + 1));
+if (s == NULL)
 return (NULL);
-}
-
-for (k = 0; k <= i; k++)
-{
-*(ptr + k) = *(s1 + l);
-l++;
-}
-for (q = 0; q < n; q++)
-{
-*(ptr + q + i) = *(s2 + m);
-m++;
-}
-*(ptr + q + i) = '\0';
-return (ptr);
+for (k = 0; k < i; k++)
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[k + i] = s2[k];
+s[i + j] = '\0';
+return (s);
 }
