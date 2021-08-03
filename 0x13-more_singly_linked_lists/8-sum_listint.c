f57ac30c9f19ab37e0907ddd3db570e9a8049443
Oluwatobi Abass <72945520+Tobi-Archademy@users.noreply.github.com>
2021-08-03 17:16:48 +0100

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,51 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-/**
- * insert_nodeint_at_index - inserts node at given index, preserving list
- * @head: head of list
- * @idx: index to put node at
- * @n: integer data for new node
- *
- * Return: pointer to new node at specified index, NULL if not found
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-unsigned int ctr = 0;
-listint_t *temp_h, *new_node;
 
-if (!head || !*head) /* EMPTY list */
+/**
+ * sum_listint - returns the sum of all the data of the linked list.
+ * @head: Double pointer to the actual position of the linked list (struct)
+ *
+ * Return: Sum of the value of all nodes.
+ */
+int sum_listint(listint_t *head)
 {
-return (NULL);
-}
-temp_h = *head;
-if (idx == 0) /* insert as first element */
-{
-new_node = malloc(sizeof(listint_t));
-}
-if (!new_node)
-{
-return (NULL);
-}
-new_node->n = n; /* set data */
-new_node->next = temp_h;
-*head = new_node;
-return (new_node);
-}
-while (temp_h)
-{
-if (ctr + 1 == idx) /* insert node here */
-{
-new_node = malloc(sizeof(listint_t));
-if (!new_node)
-{
-return (NULL);
-}
-new_node->n = n; /* set data */
-new_node->next = temp_h->next;
-temp_h->next = new_node;
-return (new_node);
-}
-temp_h = temp_h->next;
-ctr++;
-}
-return (NULL); /* didn't find node */
+	int sum = 0;
+	listint_t *tmp = head;
+
+	if (tmp == NULL)
+		return (0);
+
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
 }
