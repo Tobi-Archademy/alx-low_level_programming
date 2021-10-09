9e76c65bb459642b6b0e77fda2666f0578b5d687
Oluwatobi <oluwatobiabass5@gmail.com>
2021-10-09 06:19:25 -0700

    added a function that prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- *  * hash_table_print - prints a hash table
- *   * @ht: hash table to print
- *    *
- *     * Return: void
- *      */
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
+ *
+ * Return: void
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
